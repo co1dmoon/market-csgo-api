b9b7b6d8237e7b13f7ada96393243f265273a951
Ruslan Makarov <co1dmoon@pm.me>
2021-05-31 16:06:00 +0400

    Update MarketApi.js

--- src/MarketApi.js
+++ src/MarketApi.js
@@ -1045,9 +1045,12 @@ class MarketApi {
      * @return {Promise}
      */
     sellMassUpdatePriceById(prices, gotOptions = null) {
-    
+        let list = {};
+        for(let ui_id in prices) {
+            list[ui_id] = Number(prices[ui_id]);
+        }
 
-        return this.callPostMethodWithKey('MassSetPriceById', prices, gotOptions);
+        return this.callPostMethodWithKey('MassSetPriceById', list, gotOptions);
     }
 
     /**
